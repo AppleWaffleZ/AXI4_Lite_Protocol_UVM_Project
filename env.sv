8974d14a36be3df739255a168db249373d144be7
AppleWaffleZ <mhd4niall2102@gmail.com>
2026-07-16 10:14:01 +0800

    Adding coverage to the environment
    Added address coverage component to axi4lite_env.

--- env.sv
+++ env.sv
@@ -1,14 +1,12 @@
-// ============================================================
-// env.sv
-// Depends on: agent.sv, scoreboard.sv
-// Must compile BEFORE: test.sv
-// ============================================================
-
+// ------------------------------------------------------------
+// Environment
+// ------------------------------------------------------------
 class axi4lite_env extends uvm_env;
     `uvm_component_utils(axi4lite_env)
 
-    axi4lite_agent       agent;
-    axi4lite_scoreboard  scbd;
+    axi4lite_agent       	agent;
+    axi4lite_scoreboard  	scbd;
+  	axi4lite_addr_coverage	cov;
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
@@ -18,9 +16,12 @@ class axi4lite_env extends uvm_env;
         super.build_phase(phase);
         agent = axi4lite_agent::type_id::create("agent", this);
         scbd  = axi4lite_scoreboard::type_id::create("scbd", this);
+      	cov   = axi4lite_addr_coverage::type_id::create("cov", this);
     endfunction
 
     function void connect_phase(uvm_phase phase);
         agent.mon.ap.connect(scbd.analysis_export);
+      	agent.mon.ap.connect(cov.analysis_export);
     endfunction
 endclass
+
